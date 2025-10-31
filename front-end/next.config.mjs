2c6af70e3f9456c8da404a7feab705b5b4f1b08d
Ethan <tranvanhaubg2003@gmail.com>
2025-10-31 15:25:12 +0700

    update: env

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import envsConfig from './envs.js';
-
-const envs = envsConfig[process.env.MODE];
-if (envs) {
-  console.log(`MODE: ${process.env.MODE}`);
-  Object.keys(envs).forEach((key) => {
-    process.env[key] = envs[key];
-  });
-  console.log(`Website Url: ${process.env.NEXT_PUBLIC_FE_URL}`);
-}
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Removed to allow dynamic routing
   trailingSlash: false,
   images: {
     unoptimized: true,
